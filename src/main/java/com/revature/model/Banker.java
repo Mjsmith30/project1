b0b0d6ccaa88be1e03a30ce8272601b6de85f10f
Marques Smith <marquesjs21@yahoo.com>
2021-12-29 01:03:13 -0800

    jdbc

--- src/main/java/com/revature/model/Banker.java
+++ src/main/java/com/revature/model/Banker.java
@@ -1,9 +1,58 @@
 package com.revature.model;
 
+import java.util.Objects;
+
 public class Banker {
 	
 	private int id;
 	private String name;
 	private int account;
+	public Banker() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Banker(int id, String name, int account) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.account = account;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAccount() {
+		return account;
+	}
+	public void setAccount(int account) {
+		this.account = account;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(account, id, name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Banker other = (Banker) obj;
+		return account == other.account && id == other.id && Objects.equals(name, other.name);
+	}
+	@Override
+	public String toString() {
+		return "Banker [id=" + id + ", name=" + name + ", account=" + account + "]";
+	}
 
 }
--- src/main/java/com/revature/model/Chase.java
+++ src/main/java/com/revature/model/Chase.java
@@ -1,9 +1,52 @@
 package com.revature.model;
 
+import java.util.Objects;
+
 public class Chase{
 	
 	private int id;
 	private String specialty;
 	private String name;
+	public Chase() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSpecialty() {
+		return specialty;
+	}
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, specialty);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Chase other = (Chase) obj;
+		return id == other.id && Objects.equals(name, other.name) && Objects.equals(specialty, other.specialty);
+	}
+	@Override
+	public String toString() {
+		return "Chase [id=" + id + ", specialty=" + specialty + ", name=" + name + "]";
+	}
 
 }
